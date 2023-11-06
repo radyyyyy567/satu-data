fee70a232d36b4a836e7b0fe124b611f510b3904
radyyyyy567 <127324238+radyyyyy567@users.noreply.github.com>
2023-11-06 09:47:41 +0700

    ganti nama file photo-2

--- app/img/index.tsx
+++ app/img/index.tsx
@@ -1,5 +1,5 @@
 import logoSatuData from "./logo-satu-data.png";
-import logoKotaBatam from "./Lambang_Kota_Batam.png";
+import logoKotaBatam from "./lambang_kota_batam.png";
 import bannerWeb from "./banner_web.jpg";
 import badge from "./badge_647.png";
 import logoSatuDataIndonesia from "./logo-satu-data-indonesia.png";
